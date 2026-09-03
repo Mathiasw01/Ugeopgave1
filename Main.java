7789d76225d4e5afe626e36ffe56aa623fa9a89b
MathiasW <s224293@student.dtu.dk>
2026-09-03 22:06:22 +0200

    Opgave 1-15

---
+++ Main.java
@@ -0,0 +1,144 @@
+public class Main {
+    public static void main(String[] args) {
+        /*opgave 1
+        double price1 =25.50;
+        double price2 =15.00;
+        double price3 =30.00;
+        double subtotal=price1+price2+price3;
+        double tax=25.00;
+        double total=subtotal+subtotal*(tax/100);
+        System.out.println(subtotal+"\n"+total);
+         */
+        /*opgave 2
+
+        double foodPrice=450.00;
+        double drinkPrice=120.00;
+        double tip=50.00;
+        int numberOfPeople=4;
+        double totalBill=foodPrice+drinkPrice+tip;
+        double pricePerPerson=totalBill/numberOfPeople;
+        System.out.println(pricePerPerson);
+         */
+        /*opgave3
+
+        double originalPrice=300.00;
+        double discount=20.00;
+        double discountPrice=300*((100-discount)/100);
+        double tax=25.00;
+        double priceAfterTax=discountPrice+discountPrice*tax/100;
+        System.out.println(originalPrice);
+        System.out.println(discountPrice);
+        System.out.println(priceAfterTax);
+
+         */
+        /*opgave 4
+        int numberOfGuests=15;
+        int numberOfCupcakes=40;
+        int cupcakesPerGuest=numberOfCupcakes/numberOfGuests;
+        int leftoverCupcakes=numberOfCupcakes%numberOfGuests;
+        System.out.println(cupcakesPerGuest);
+        System.out.println(leftoverCupcakes);
+
+         */
+        /*opgave 5
+        StringBuilder sb= new StringBuilder();
+        String name="Anna";
+        int age=25;
+        sb.append("Hello, ");
+        sb.append(name);
+        sb.append("! You are ");
+        sb.append(age);
+        sb.append(" Years old.");
+        System.out.println(sb.toString());
+
+         */
+        /*opgave 6
+        int score=100;
+        System.out.println(score);
+        score+=50;
+        System.out.println(score);
+        score+=25;
+        System.out.println(score);
+
+         */
+        /*Opgave 7
+        int health =100;
+        health-=15;
+        System.out.println(health);
+        health-=20;
+        System.out.println(health);
+
+         */
+        /*Opgave 8
+        double price=50.00;
+        System.out.println(price);
+        price*=2;
+        System.out.println(price);
+         */
+        /*Opgave 9
+        int counter=10;
+        System.out.println(counter);
+        counter++;
+        System.out.println(counter);
+        counter--;
+        System.out.println(counter);
+         */
+        /*Opgave 10
+        int number=10;
+        number+=5;
+        System.out.println(number);
+        number-=3;
+        System.out.println(number);
+        number++;
+        System.out.println(number);
+        number--;
+        System.out.println(number);
+
+         */
+        /*Opgave 11
+        String productName="Laptop";
+        double price=7999.99;
+        boolean isInStock=true;
+        int remainingStock=15;
+        double percentDiscount=10.00;
+        String itemCategory="Electronics";
+         */
+        /*Opgave 12
+        String studentName="Anna";
+        int testScore=85;
+        int assignmentScore=92;
+        double finalScore=88.5;
+        boolean hasPassed=true;
+         */
+        /*Opgave 13
+        String guestName="Peter Hansen";
+        int roomNumber=204;
+        int numberOfNights=3;
+        double pricePerNight=899.00;
+        boolean hasBreakfast=true;
+        double totalPrice=2697.00;
+         */
+        /* opgave 14
+        int dailySteps=8547;
+        int dailyStepGoal=10000;
+        double distanceInKilometers=6.2;
+        int caloriesBurned=420;
+        boolean hasReachedGoal=false;
+
+         */
+        /*Opgave 15
+        String bookTitle;
+        String author;
+        int isbn;
+        int noPages;
+        boolean isAvailable;
+        String currentBorrower;
+        String dueDate;
+        double lateFee;
+        boolean isBestSeller;
+        double bookRating;
+
+         */
+    }
+
+}
